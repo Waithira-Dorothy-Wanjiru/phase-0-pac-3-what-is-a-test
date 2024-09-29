bedae4d377a43a3af8ab415092c00ec4d69034a9
Dorothy <dorothywanjiru32@gmail.com>
2024-09-29 13:57:20 +0300

    pac 3 what is a test

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
-
-module.exports = {name, height, message};
+const { name, height, message } = require("../index.js");
+describe("Name", () => {
+    if('returns "Susan"', () => {
+      expect(name).toEqual("Susan");
+    });
+  });
+  describe("Height", () => {
+    if("is less than 40", () => {
+      expect(height).toBeLessThan(40);
+    });
+  });
+  describe("Message", () => {
+    if("gives the name and height", () => {
+      expect(message).toInclude(name);
+      expect(message).toInclude(height);
+    });
+  });
